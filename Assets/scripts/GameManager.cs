437e004c7b08f0e8c7e7f75a47c4f38c9e35f1f3
IMAN-00 <imanalawneh928@gmail.com>
2024-11-06 09:08:32 +0200

    important

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    void Start()
+    {
+        Player red = new Player(1);
+        Player yellow = new Player(2);
+        InitializePlayer()new player(1);
+    }
+
+
+        
+
+}
--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -4,29 +4,32 @@ using UnityEngine;
 
 public class Player
 {
- public string name = " ";
- private int health = 100;
- static public int PlayerCount = 0;
-}
-public void InitializePlayer()
-{
-    PlayerName = name;
-    health = InitialHealth;
-    return PlayerName;
-    PlayerCount++;
-}
-public void Heal(int amount)
-{
-    healh += amount;
-    Debug.Log(health = health + amount);
-}
-public void FullRestore(bool)
-{
-    if (FullRestore) 
-    health = 100;
-    Debug.log( PlayerName + "current health" + health );
-}
-static public int PlayerCount()
-{
-    Debug.log(players: " " + PlayerCount);
+
+    public string name;
+    public int health = 100;
+    static public int PlayerCount = 0;
+ 
+    public void Player(string PlayerName)
+      {
+          name = PlayerName ;
+         health = 100;
+         PlayerCount++;
+      }
+      
+    public void Heal(int amount)
+      {
+         healh += amount;
+         Debug.Log( PlayerName + "healed" + amount + "points.current health:" + health);
+      }
+    public void Heal(bool FullRestore)
+      {
+         if (FullRestore) 
+         health = 100;
+         Debug.Log("Players" + PlayerName + "current health" + health );
+      }
+    static public int ShowPlayerCount()
+      {
+         Debug.Log( "Total players:" +PlayerCount);
+      }
+
 }
